fadbb90de6becabc10dc51301d04b574717fc0aa
Gwendoline Sanabria <gwensanabria@gmail.com>
2020-05-16 23:42:51 -0500

    timer resets on restart, no longer stops at zero

---
+++ assets/questions.js
@@ -0,0 +1,80 @@
+const questions = [
+    {
+        question: 'An array is enclosed in:',
+        answers: [
+            {text: 'curly brackets', correct: false},
+            {text: 'quotes', correct: false},
+            {text: 'square brackets', correct: true},
+            {text: 'parentheses', correct: false}
+        ]
+    },
+
+    {
+        question: 'Which tag creates an ordered list?',
+        answers: [
+            {text: '<li>', correct: false},
+            {text: '<ul>', correct: false},
+            {text: '<list>', correct: false},
+            {text: '<ol>', correct: true}
+        ]
+    },
+
+    {
+        question: 'Strings are enclosed in:',
+        answers: [
+            {text: 'parentheses', correct: false},
+            {text: 'square brackets', correct: false},
+            {text: 'quotes', correct: true},
+            {text: '<>', correct: false}
+        ]
+    },
+
+    {
+        question: 'What does NaN stand for?',
+        answers: [
+            {text: 'Not-available-Now', correct: false},
+            {text: 'Now-among-Numbers', correct: false},
+            {text: 'Not-a-Number', correct: true}
+        ]
+    },
+
+    {
+        question: 'Where do you link the css?',
+        answers: [
+            {text: '<body>', correct: false},
+            {text: '<head>', correct: true},
+            {text: '<section>', correct: false},
+            {text: '<footer>', correct: false}
+        ]
+    },
+
+    {
+        question: 'What does CSS stand for?',
+        answers: [
+            {text: 'Counter-Strike: Source', correct: false},
+            {text: 'Cascading Style Sheets', correct: true},
+            {text: 'Combat Service Support', correct: false},
+            {text: 'College of Social Sciences', correct: false}
+        ]
+    },
+
+    {
+        question: 'In JavaScript, what element is used to store multiple values in a single variable?',
+        answers: [
+            {text: 'Arrays', correct: true},
+            {text: 'Strings', correct: false},
+            {text: 'Functions', correct: false},
+            {text: 'Loop', correct: false}
+        ]
+    },
+
+    {
+        question: 'Which HTML tag is self closing?',
+        answers: [
+            {text: '<script>', correct: false},
+            {text: '<header>', correct: false},
+            {text: '<img>', correct: true},
+            {text: '<html>', correct: false}
+        ]
+    }
+]
--- assets/script.js
+++ assets/script.js
@@ -7,145 +7,119 @@
 // show high/low score
 // save to local storage
 
-
-// CHECK TO SEE IF I NEED CORRECT AND WRONG IN SOME PARTS. NOT USING COLOR
-
 const startBtn = document.getElementById("start-btn");
 const nextBtn = document.getElementById("next-btn");
 const questionContainerEl = document.getElementById("question-container");
 const questionEl = document.getElementById("question");
 const answerBtnEl = document.getElementById("answer-buttons");
-const time001 = document.getElementById('time001');
-const timeUp = document.getElementById('time-up');
+const time = document.getElementById("time");
+const timeUp = document.getElementById("time-up");
 
-
-let randomQ, currentQIndex
-
-startBtn.addEventListener('click', startGame)
-nextBtn.addEventListener('click', () => {
-    currentQIndex++
-    setNextQ()
-})
-// Start button, randomize question
+let randomQ, currentQIndex;
+////////// increments by one ////////////////////
+startBtn.addEventListener("click", startGame);
+startBtn.addEventListener("click", timer => {
+    update = setInterval("timer()", 1000)
+});
+nextBtn.addEventListener("click", () => {
+  currentQIndex++;
+  setNextQ();
+});
+///////////////////////////// Start button, randomize question //////////////////////////
 function startGame() {
-    c = 150
-    // console.log('Start')
-    startBtn.classList.add('hide') // hides starts button when pressed
-    randomQ = questions.sort(() => Math.random() - .5)
-    currentQIndex = 0
-    questionContainerEl.classList.remove('hide') // Unhides the question, answers
-    setNextQ()
+  t = 100;
+  // console.log('Start')
+  startBtn.classList.add("hide"); // hides starts button when pressed
+  randomQ = questions.sort(() => Math.random() - 0.5);
+  currentQIndex = 0;
+  questionContainerEl.classList.remove("hide"); // Unhides the question, answers
+  setNextQ();
 }
 // console.log(startGame)
 
-// Timer
-function timer001() {
-    c = c - 1;
-    if (c < 150) {
-        time001.innerHTML = c;
-    }
-    if (c < 1) {
-        window.clearInterval(update);
-        startBtn.innerText = 'Restart'
-        startBtn.classList.remove('hide')
-        timeUp.innerText = "Time's Up!"
-        timeUp.classList.remove('hide')
-    }
+////////////////////////// Timer //////////////////////////////////
+function timer() {
+  t = t - 1;
+  if (t < 100) {
+    time.innerHTML = t;
+  }
+  if (t < 1) {
+    window.clearInterval(update);
+    startBtn.innerText = "Restart";
+    startBtn.classList.remove("hide");
+    timeUp.innerText = "Time's Up!";
+    timeUp.classList.remove("hide");
+  }
 }
 
-update = setInterval('timer001()', 1000);
+update = setInterval("timer()", 1000);
 
-
-
-
-
+/////////////////// Set Next Question ///////////////////////////////////
 function setNextQ() {
-    resetState()
-    showQ(randomQ[currentQIndex])
+  resetState();
+  showQ(randomQ[currentQIndex]);
 }
 
 function showQ(question) {
-    questionEl.innerText = question.question
-    question.answers.forEach(answer => {
-        const button = document.createElement('button')
-        button.innerText = answer.text
-        button.classList.add('btn')
-        if (answer.correct) {
-            button.dataset.correct = answer.correct
-        }
-        button.addEventListener('click', selectAnswer)
-        answerBtnEl.appendChild(button)
-    });
+  questionEl.innerText = question.question;
+  question.answers.forEach((answer) => {
+    const button = document.createElement("button");
+    button.innerText = answer.text;
+    button.classList.add("btn");
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
+    answerBtnEl.appendChild(button);
+  });
 }
 
+//problem reseting timer on reset
 function resetState() {
-    nextBtn.classList.add('hide')
-    while (answerBtnEl.firstChild) {
-        answerBtnEl.removeChild(answerBtnEl.firstChild)
-    }
+  clearStatusClass(document.body);
+  nextBtn.classList.add("hide");
+  timeUp.classList.add("hide");
+  while (answerBtnEl.firstChild) {
+    answerBtnEl.removeChild(answerBtnEl.firstChild);
+  }
 }
 
 function selectAnswer(e) {
-    const selectedBtn = e.target
-    const correct = selectedBtn.dataset.correct
-    setStatusClass(document.body, correct)
-    Array.from(answerBtnEl.children).forEach(button => {
-        setStatusClass(button, button.dataset.correct)
-    })
-    if (randomQ.length > currentQIndex + 1) {
-        nextBtn.classList.remove('hide')
-    } else {
-        window.clearInterval(update);
-        c ='-';
-        startBtn.innerText = 'Restart'
-        startBtn.classList.remove('hide')
-        timeUp.innerText = "Finished!"
-        timeUp.classList.remove('hide')
-    }
+  const selectedBtn = e.target;
+  const correct = selectedBtn.dataset.correct;
+  setStatusClass(document.body, correct);
+  Array.from(answerBtnEl.children).forEach((button) => {
+    setStatusClass(button, button.dataset.correct);
+  });
+  if (randomQ.length > currentQIndex + 1) {
+    nextBtn.classList.remove("hide");
+  } else {
+    window.clearInterval(update);
+    t = "-";
+    startBtn.innerText = "Restart";
+    startBtn.classList.remove("hide");
+    timeUp.innerText = "Finished!";
+    timeUp.classList.remove("hide");
+  }
 }
 
 function setStatusClass(element, correct) {
-    clearStatusClass(element)
-    if (correct) {
-        element.classList.add('correct')
-    } else {
-        element.classList.add('wrong')
-    }
+  clearStatusClass(element);
+  if (correct) {
+    element.classList.add("correct");
+  } else {
+    element.classList.add("wrong");
+  }
+  /// close to subtracting time, higher number than i want though///
+  if (!correct) {
+    element.classList.add("wrong");
+    return (t = t - parseInt(2));
+  } else {
+    element.classList.add("correct");
+  }
 }
 
 function clearStatusClass(element) {
-    element.classList.remove('correct')
-    element.classList.remove('wrong')
+  element.classList.remove("correct");
+  element.classList.remove("wrong");
 }
-
-const questions = [
-    {
-        question: 'What is 2 + 2',
-        answers: [
-            {text: '4', correct: true},
-            {text: '22', correct: false},
-            {text: '44', correct: false},
-            {text: '24', correct: false}
-        ]
-    },
-
-    {
-        question: 'What is 2 + 2',
-        answers: [
-            {text: '4', correct: true},
-            {text: '22', correct: false},
-            {text: '44', correct: false},
-            {text: '24', correct: false}
-        ]
-    },
-
-    {
-        question: 'What is 2 + 2',
-        answers: [
-            {text: '4', correct: true},
-            {text: '22', correct: false},
-            {text: '44', correct: false},
-            {text: '24', correct: false}
-        ]
-    }
-]
